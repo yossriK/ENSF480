4134e705242ccee466499226a265b13e95182559
yossriK <46327024+yossriK@users.noreply.github.com>
2019-11-20 23:10:22 -0700

    Update Landlord.java

--- server/model/Landlord.java
+++ server/model/Landlord.java
@@ -4,19 +4,31 @@ public class Landlord  extends Person{
 
 	
 	
+	public Landlord(Name n, Address ad, BirthDate bd) {
+		super(n, ad, bd);
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
 	// when opted, it creates new property, gotta have arguments
-	public Property createProperty() {
+	 * this will be called from controller and populated using gui stuff	
+	*/
+	public Property createProperty(String pT, int nb, int nw, boolean fur, String cq) {
+		Property p= new Property(pT, nb,  nw,  fur, cq);
 		
+		return p;
 		
-		return null;
 	}
 	
-	
+	/*
+	 * taking the data of property instance and add it to data base.
+	 * 
+	 */
 	public void registerProperty(Property p) {
 		
 		
 		
-		
+	
 		
 	}
 	
@@ -26,4 +38,9 @@ public class Landlord  extends Person{
 		
 		
 	}
+public void printtest() {
+		
+		System.out.println(" in Landlord");
+	}
+
 }
